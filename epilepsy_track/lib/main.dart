e0162030224a00d8fc558f7c4ef41537d67ae44d
bhong928 <benhong928@gmail.com>
2023-03-11 23:09:44 -0500

    overrides

--- epilepsy_track/lib/main.dart
+++ epilepsy_track/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -41,37 +40,73 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-        @override
-        Widget build (BuildContext cotext){
-        return Scaffold(
-          body: SfCalendar(
-            view: CalendarView.month,
-            firstDayOfWeek: 7,
-            //initialDisplayDate: DateTime(2023,03,11,12,00),
-            dataSource: EpilepsyLogging(getAppointments()),
-          ),
-        );
-        }
+  @override
+  Widget build(BuildContext cotext) {
+    return Scaffold(
+      body: SfCalendar(
+        view: CalendarView.month,
+        firstDayOfWeek: 7,
+        //initialDisplayDate: DateTime(2023,03,11,12,00),
+        dataSource: EpilepsyLogging(getAppointments()),
+      ),
+    );
   }
+}
 
-  List<Appointment> getAppointments(){
-    List<Appointment> meetings=<Appointment>[];
-    final DateTime today = DateTime.now();
-    final DateTime startTime = DateTime(today.year, today.month, today.day, 12,0,0); //12,0,0 is 6 pm
-    final DateTime endTime = startTime.add(const Duration(hours:2));
+List<Appointment> getAppointments() {
+  List<Appointment> meetings = <Appointment>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+      DateTime(today.year, today.month, today.day, 12, 0, 0); //12,0,0 is 6 pm
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
 
-    meetings.add(Appointment(
+  meetings.add(Appointment(
       startTime: startTime,
       endTime: endTime,
       subject: 'Take Medidcation',
       color: Colors.blue,
-    recurrenceRule: 'FREQ=DAILY;COUNT=365'));
+      recurrenceRule: 'FREQ=DAILY;COUNT=365'));
 
-    return meetings;
-  }
-
-  class EpilepsyLogging extends CalendarDataSource{
-EpilepsyLogging(List<Appointment> source){
-  appointments = source;
+  return meetings;
 }
+
+class EpilepsyLogging extends CalendarDataSource {
+  EpilepsyLogging(List<Appointment> source) {
+    appointments = source;
   }
+
+  @override
+  DateTime getStartTime(int index) {
+    return appointments![index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments![index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments![index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments![index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].isAllDay;
+  }
+}
+
+class Meeting {
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  String eventName;
+  DateTime from;
+  DateTime to;
+  Color background;
+  bool isAllDay;
+}
